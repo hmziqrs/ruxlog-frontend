2d64e398b622c131bb64e5e6ad025c533c0eb5e0
hmziqrs <hmziqrs@gmail.com>
2024-10-21 04:45:53 +0500

    paek-update

---
+++ _templates/store/update/prompt.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's the name of the existing store? (e.g., user, auth)"
+  },
+  {
+    type: 'input',
+    name: 'newState',
+    message: "What's the name of the new state to add?"
+  }
+];
