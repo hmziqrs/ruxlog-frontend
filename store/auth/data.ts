2b737d6e1994a7d4cbe643577340d3b81217765c
hmziqrs <hmziqrs@gmail.com>
2024-10-21 17:48:44 +0500

    fixed type

--- store/auth/data.ts
+++ store/auth/data.ts
@@ -3,11 +3,23 @@ import { AuthState } from './types';
 
 export const authState: AuthState = {
   state: {
+    logxa: { ...subState },
+
+    logxx: { ...subState },
+
+    logx: { ...subState },
+
     log: { ...subState },
 
     login: { ...subState }
   },
   data: {
+    logxa: null,
+
+    logxx: null,
+
+    logx: null,
+
     log: null,
 
     login: null
