1bf5b29435614000ee52eb459fbfec05215de258
hmziqrs <hmziqrs@gmail.com>
2024-10-21 15:33:42 +0500

    Fixed actions, data templates

--- store/auth/actions.ts
+++ store/auth/actions.ts
@@ -21,6 +21,23 @@ export const login = (set: ImmerAction<AuthStore>) => async () => {
   }
 };
 
+export const log = (set: ImmerAction<AuthStore>) => async () => {
+  set((state) => {
+    state.state.log = { ...subState, loading: true };
+  });
+  try {
+    // Add your API call here
+    set((state) => {
+      state.state.log = { ...subState, success: true };
+      // Update state.data.log here
+    });
+  } catch (error) {
+    set((state) => {
+      state.state.log = { ...subState, error: true };
+    });
+  }
+};
+
 export const reset = (set: ImmerAction<AuthStore>) => async () => {
   set((state) => {
     state.state = { ...authState.state };
