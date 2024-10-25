4ffffb9bbc5c8ce80dc79e2202afad23c0a94438
hmziqrs <hmziqrs@gmail.com>
2024-10-25 14:42:47 +0500

    hygen templates

---
+++ admin/_templates/store/new/prompt.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's the name of the store? (e.g., user, auth)"
+  },
+  {
+    type: 'input',
+    name: 'states',
+    message:
+      'Enter the state names (comma-separated, e.g., details,referrals,token,signup)'
+  }
+];
