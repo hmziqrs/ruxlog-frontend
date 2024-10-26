8d67df5dca7e2f85ed8233ecc00403e43c058ed7
hmziqrs <hmziqrs@gmail.com>
2024-10-26 06:17:40 +0500

    improved breadcrumbs

--- admin/src/app/(dashboard)/brain.tsx
+++ admin/src/app/(dashboard)/brain.tsx
@@ -1,39 +1,36 @@
 import { usePathname } from 'next/navigation';
 import { useMemo } from 'react';
 
+// if (pathname === '/') {
+//   return [
+//     {
+//       href: '/',
+//       label: 'Home',
+//       isLast: true,
+//     },
+//   ];
+// }
 export function useDashboardLayout() {
   const pathname = usePathname();
 
   const breadcrumbs = useMemo(() => {
-    if (pathname === '/') {
-      return [
-        {
-          href: '/',
-          label: 'Home',
-          isLast: true,
-        },
-      ];
-    }
-    // Remove first empty string from split
-    const segments = pathname.split('/').filter(Boolean);
+    const segments = pathname
+      .split('/')
+      .filter(Boolean)
+      .map((segment) => segment.toLowerCase());
 
     return segments.map((segment, index) => {
       const href = `/${segments.slice(0, index + 1).join('/')}`;
-      const label = segment
-        // Add space between camelCase
-        .replace(/([A-Z])/g, ' $1')
-        // Capitalize first letter
-        .replace(/^./, (str) => str.toUpperCase())
-        // Replace hyphens and underscores with spaces
-        .replace(/[-_]/g, ' ');
-
+      const label = segment.charAt(0).toUpperCase() + segment.slice(1);
       return {
-        href,
         label,
+        href,
         isLast: index === segments.length - 1,
       };
     });
   }, [pathname]);
 
-  return { breadcrumbs };
+  return {
+    breadcrumbs,
+  };
 }
