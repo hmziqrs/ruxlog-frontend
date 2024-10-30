5dc8a0fd5e89e5f4915b4fe45554fc15c59a546b
hmziqrs <hmziqrs@gmail.com>
2024-10-31 00:25:55 +0500

    layout complete

--- client/src/app/(blog)/layout.tsx
+++ client/src/app/(blog)/layout.tsx
@@ -1,17 +1,19 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { ThemeToggle } from '@/components/theme-toggle';
+import { User } from 'lucide-react';
+import { SiGithub } from '@icons-pack/react-simple-icons';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header className="dark:bg-zinc-900/50 p-4">
-        <div className="container mx-auto flex items-center">
+      <header className="dark:bg-zinc-900/50 bg-zinc-100/50 p-4">
+        <div className="container mx-auto flex items-center justify-center">
           <Link href="/" className="flex justify-center items-center">
             <Image src="/logo.png" width={60} height={60} alt="logo" />
           </Link>
           <div className="flex-grow" />
-          <div className="space-x-4 text-md">
+          <div className="space-x-4 text-md flex items-center">
             {/* <Link href="/" className="hover:underline">
               Home
             </Link> */}
@@ -21,13 +23,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <Link href="/contact" className="hover:underline">
               Contact
             </Link>
-            <span>Profile Icon</span>
-            <ThemeToggle />
+
+            <User size={20} className="cursor-pointer hover:text-zinc-400 " />
+            <SiGithub
+              size={20}
+              className="cursor-pointer hover:text-zinc-400 "
+            />
+            <ThemeToggle className="cursor-pointer hover:text-zinc-400 " />
           </div>
         </div>
       </header>
       <div className="flex flex-grow container mx-auto">{children}</div>
-      <footer className="dark:bg-zinc-900/50 p-8">
+      <footer className="dark:bg-zinc-900/50 bg-zinc-100/50 p-8">
         <div className="container mx-auto flex flex-col items-center">
           <div className="font-mono text-sm">
             &copy; {new Date().getFullYear()}, Built with Tailwind CSS, Next.js
@@ -43,6 +50,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             </Link>
             <Link href="#" className="hover:underline">
               Sitemap
+            </Link>
+            <Link href="#" className="hover:underline">
+              Github
             </Link>
             <Link href="#" className="hover:underline">
               RSS
