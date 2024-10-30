35417c9ce3115216327cf330cc35838e8b0c3737
hmziqrs <hmziqrs@gmail.com>
2024-10-30 22:24:21 +0500

    footer

--- client/src/app/(blog)/layout.tsx
+++ client/src/app/(blog)/layout.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header className="bg-zinc-900/50 text-white p-4">
+      <header className="bg-zinc-900/50 p-4">
         <div className="container mx-auto flex justify-between items-center">
           <div className="flex items-center space-x-4">
-            <div className="text-lg font-bold">Logo</div>
+            <div className="text-lg">Geist</div>
             <div className="flex space-x-4">
               <a href="#" className="hover:underline">
                 About
@@ -20,19 +22,26 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </div>
       </header>
       <div className="flex flex-grow container mx-auto">{children}</div>
-      <footer className="bg-zinc-900/50 text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="flex space-x-4">
-            <a href="#" className="hover:underline">
-              Sitemap
-            </a>
-            <a href="#" className="hover:underline">
-              RSS
-            </a>
+      <footer className="bg-zinc-900/50 p-8">
+        <div className="container mx-auto flex flex-col items-center">
+          <div className="font-mono text-sm">
+            &copy; {new Date().getFullYear()}, Built with Tailwind CSS, Next.js
+            and ❤️ by hmziqrs
           </div>
-          <div className="text-sm">
-            &copy; {new Date().getFullYear()} Built with Tailwind CSS and
-            Next.js by hmziqrs
+          <div className="h-4" />
+          <div className="space-x-4 text-xs dark:text-zinc-700">
+            <Link href="#" className="hover:underline">
+              Privacy Policy
+            </Link>
+            <Link href="#" className="hover:underline">
+              Terms of Service
+            </Link>
+            <Link href="#" className="hover:underline">
+              Sitemap
+            </Link>
+            <Link href="#" className="hover:underline">
+              RSS
+            </Link>
           </div>
         </div>
       </footer>
