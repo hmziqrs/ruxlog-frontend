f7f28153f5b2f2621ae0708e1632c115234ed735
hmziqrs <hmziqrs@gmail.com>
2024-10-31 22:46:18 +0500

    Samewhat fix

---
+++ client/src/types.ts
@@ -0,0 +1,37 @@
+export interface Author {
+  avatar: string | null;
+  email: string;
+  id: number;
+  name: string;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Tag {
+  id: number;
+  name: string;
+}
+
+export interface Post {
+  author: Author;
+  authorId: number;
+  category: Category | null;
+  categoryId: number | null;
+  content: string;
+  createdAt: string;
+  excerpt: string;
+  featuredImageUrl: string | null;
+  id: number;
+  isPublished: boolean;
+  likesCount: number;
+  publishedAt: string | null;
+  slug: string;
+  tagIds: number[];
+  tags: Tag[];
+  title: string;
+  updatedAt: string;
+  viewCount: number;
+}
