75f726f73e12c6c335a5d2bd29b4f7636d272a0e
hmziqrs <hmziqrs@gmail.com>
2024-11-03 07:49:29 +0500

    Improved more UI

--- client/src/components/MetaPill.tsx
+++ client/src/components/MetaPill.tsx
@@ -8,7 +8,7 @@ interface MetaPillProps {
 
 export function MetaPill({ icon: Icon, label, suffix }: MetaPillProps) {
   return (
-    <span className="inline-flex items-center gap-2 sm:gap-3 sm:px-4 sm:py-2 px-2.5 py-1.5 bg-zinc-100 dark:bg-zinc-800 rounded-md">
+    <span className="inline-flex items-center text-xs sm:text-sm gap-2 sm:gap-3 sm:px-4 sm:py-2 px-2.5 py-1.5 bg-zinc-100 dark:bg-zinc-800 rounded-md">
       <Icon className="w-3.5 h-3.5 sm:w-4 sm:h-4" />
       <span>
         {label}
