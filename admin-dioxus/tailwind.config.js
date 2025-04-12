9d7b54bbadeefb5843c0e5659ee434f4123508c1
hmziqrs <hmziqrs@gmail.com>
2025-04-12 23:26:40 +0500

    A very basic root structure.

--- admin-dioxus/tailwind.config.js
+++ admin-dioxus/tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "all",
-  content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html"],
+  content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html", "./index.html"],
   theme: {
     extend: {},
+  },
+  theme: {
+    extend: {
+      screens: {
+        xs: "410px",
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
 };
