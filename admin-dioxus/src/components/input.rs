a1f0cdd26efd9137c43c6f4915470dc77bd1ee02
hmziqrs <hmziqrs@gmail.com>
2025-04-13 11:28:39 +0500

    feat: Enhance input component with readonly support and onblur handler for slug generation

--- admin-dioxus/src/components/input.rs
+++ admin-dioxus/src/components/input.rs
@@ -1,7 +1,7 @@
 use crate::hooks::{OxForm, OxFormModel};
 use dioxus::prelude::*;
 
-#[derive(Props, PartialEq, Eq, Clone)]
+#[derive(Props, PartialEq, Clone)]
 pub struct AppInputProps<T: OxFormModel + 'static> {
     form: Signal<OxForm<T>>,
     name: String,
@@ -11,48 +11,73 @@ pub struct AppInputProps<T: OxFormModel + 'static> {
     placeholder: Option<String>,
     #[props(default = String::from("text"))]
     r#type: String,
+    #[props(default = None)]
+    onchange: Option<EventHandler<String>>,
+    #[props(default = None)]
+    onblur: Option<EventHandler<()>>,
+    #[props(default = None)]
+    onfocus: Option<EventHandler<()>>,
+    #[props(default = false)]
+    disabled: bool,
+    #[props(default = false)]
+    readonly: bool,
 }
 
 #[component]
-pub fn AppInput<T>(mut props: AppInputProps<T>) -> Element
+pub fn AppInput<T>(props: AppInputProps<T>) -> Element
 where
     T: OxFormModel + 'static,
 {
-    let read = props.form.read();
-    let field = read.get_field(&props.name).unwrap();
+    let mut form = props.form.clone();
+    let name = props.name.clone();
+    
+    let field = {
+        let read = form.read();
+        read.get_field(&name).unwrap().clone()
+    };
 
-    let name_on = props.name.clone();
-    let name_ob = props.name.clone();
-    let name_of = props.name.clone();
+    let name_on = name.clone();
+    let name_ob = name.clone();
+    let name_of = name.clone();
+    
+    let onchange_handler = props.onchange.clone();
+    let onblur_handler = props.onblur.clone();
+    let onfocus_handler = props.onfocus.clone();
 
     rsx! {
         div {
-            if props.label.is_some() {
-                label {
-                    class: "block text-sm font-medium text-primary, mb-2",
-                    {props.label.unwrap()}
-                }
+            if let Some(label) = &props.label {
+                label { class: "block text-sm font-medium text-primary, mb-2", {label.clone()} }
             }
             input {
-                r#type: props.r#type,
+                disabled: props.disabled,
+                readonly: props.readonly,
+                r#type: props.r#type.clone(),
                 class: "w-full px-4 py-2 input",
                 value: field.value.clone(),
-                placeholder: if props.placeholder.is_some() { props.placeholder.unwrap() },
+                placeholder: props.placeholder.clone(),
                 onchange: move |event| {
-                    props.form.write().update_field(&name_on, event.value());
+                    let value = event.value();
+                    if let Some(handler) = &onchange_handler {
+                        handler.call(value.clone());
+                    }
+                    form.write().update_field(&name_on, value);
                 },
                 onblur: move |_| {
-                    props.form.write().blur_field(&name_ob);
+                    if let Some(handler) = &onblur_handler {
+                        handler.call(());
+                    }
+                    form.write().blur_field(&name_ob);
                 },
                 onfocus: move |_| {
-                    props.form.write().focus_field(&name_of);
-                }
+                    if let Some(handler) = &onfocus_handler {
+                        handler.call(());
+                    }
+                    form.write().focus_field(&name_of);
+                },
             }
-            if field.error.is_some() {
-                p {
-                    class: "my-2 text-sm text-error",
-                    {field.error.as_ref().unwrap().to_string()}
-                }
+            if let Some(error) = &field.error {
+                p { class: "my-2 text-sm text-error", {error.clone()} }
             }
         }
     }
