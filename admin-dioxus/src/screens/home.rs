8682edb0d98ca4776f54dab7fa3e02f5d95e9309
hmziqrs <hmziqrs@gmail.com>
2025-04-13 09:41:58 +0500

    Cargo radio works but waiting on cross reducer compatibility. Might not endup using it.

--- admin-dioxus/src/screens/home.rs
+++ admin-dioxus/src/screens/home.rs
@@ -1,11 +1,26 @@
 use dioxus::prelude::*;
+use dioxus_radio::hooks::{use_radio, RadioAsyncReducer};
+
+use crate::store::{AuthState, AuthStateAction, AuthStateChannel};
 
 #[component]
 pub fn HomeScreen() -> Element {
+    let mut auth_channel = use_radio::<AuthState, AuthStateChannel>(AuthStateChannel::Main);
+    let auth_state = auth_channel.read();
+
     rsx! {
         div {
             h1 { "Welcome to Dioxus!" }
             p { "This is a simple example of a Dioxus application." }
+            p { {format!("{:?}", auth_state.login_status)} }
+            button {
+                class: "btn btn-primary", onclick: move |_| {
+                    auth_channel.async_apply(AuthStateAction::Login("".to_string(), "".to_string()));
+                // spawn(async move {
+                //     auth_channel.write().login("username".to_string(), "password".to_string()).await;
+                // });
+            }, "Simulate"
+            }
         }
     }
 }
---
+++ admin-dioxus/src/store/lib.rs
@@ -0,0 +1,82 @@
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum StateFrameStatus {
+    Init,
+    Loading,
+    Success,
+    Failed,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct StateFrame<T: Clone> {
+    pub status: StateFrameStatus,
+    pub data: Option<T>,
+    pub message: Option<String>,
+}
+
+impl<T: Clone> Default for StateFrame<T> {
+    fn default() -> Self {
+        Self {
+            status: StateFrameStatus::Init,
+            data: None,
+            message: None,
+        }
+    }
+}
+
+impl<T: Clone> StateFrame<T> {
+    pub fn new() -> Self {
+        Self {
+            status: StateFrameStatus::Init,
+            data: None,
+            message: None,
+        }
+    }
+
+    pub fn new_with_loading(message: Option<String>) -> Self {
+        Self {
+            status: StateFrameStatus::Loading,
+            data: None,
+            message,
+        }
+    }
+
+    pub fn new_with_data(data: Option<T>) -> Self {
+        Self {
+            status: StateFrameStatus::Success,
+            data,
+            message: None,
+        }
+    }
+
+    pub fn is_init(&self) -> bool {
+        self.status == StateFrameStatus::Init
+    }
+
+    pub fn is_loading(&self) -> bool {
+        self.status == StateFrameStatus::Loading
+    }
+
+    pub fn is_success(&self) -> bool {
+        self.status == StateFrameStatus::Success
+    }
+
+    pub fn is_failed(&self) -> bool {
+        self.status == StateFrameStatus::Failed
+    }
+
+    pub fn set_loading(&mut self, message: Option<String>) {
+        self.status = StateFrameStatus::Loading;
+        self.message = message;
+    }
+
+    pub fn set_success(&mut self, data: Option<T>, message: Option<String>) {
+        self.status = StateFrameStatus::Success;
+        self.data = data;
+        self.message = message;
+    }
+
+    pub fn set_failed(&mut self, message: Option<String>) {
+        self.status = StateFrameStatus::Failed;
+        self.message = message;
+    }
+}
