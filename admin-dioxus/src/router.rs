5ba1556b533a9dd097c29b5519b1c813dbad5a22
hmziqrs <hmziqrs@gmail.com>
2025-04-13 10:38:04 +0500

    Testing layouts

--- admin-dioxus/src/router.rs
+++ admin-dioxus/src/router.rs
@@ -6,10 +6,55 @@ use crate::screens::LoginScreen;
 #[derive(Debug, Clone, Routable, PartialEq)]
 #[rustfmt::skip]
 pub enum Route {
-    // #[layout(Navbar)]
+    #[layout(NavBar)]
+    #[layout(Footer)]
     #[route("/")]
     HomeScreen {},
 
     #[route("/login")]
     LoginScreen {},
+}
+
+#[component]
+fn NavBar() -> Element {
+    rsx! {
+        div {
+            class: "bg-zinc-800 text-white py-4 px-6 flex justify-between items-center shadow-lg",
+            h1 {
+                class: "text-2xl font-bold tracking-tight",
+                "Admin Dioxus"
+            }
+            ul {
+                class: "flex space-x-6",
+                li {
+                    class: "hover:text-zinc-300 transition-colors duration-200",
+                    Link {
+                        class: "font-medium",
+                        to: Route::HomeScreen {  },
+                        "Home"
+                    }
+                }
+                li {
+                    class: "hover:text-zinc-300 transition-colors duration-200",
+                    Link {
+                        class: "font-medium",
+                        to: Route::LoginScreen {  },
+                        "Login"
+                    }
+                }
+            }
+        }
+        Outlet::<Route> {}
+    }
+}
+
+#[component]
+fn Footer() -> Element {
+    rsx! {
+        Outlet::<Route> {}
+        div {
+            class: "footer",
+            "Copyright © 2025 Admin Dioxus"
+        }
+    }
 }
