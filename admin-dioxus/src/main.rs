b305aa68b51b0c0bb7562110d6416e759f8c3ce0
hmziqrs <hmziqrs@gmail.com>
2025-04-13 06:19:13 +0500

    Add dependencies and update Tailwind CSS configuration

--- admin-dioxus/src/main.rs
+++ admin-dioxus/src/main.rs
@@ -10,7 +10,22 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "stylesheet", href: asset!("/assets/tailwind.css") }
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com"
+        }
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            "crossorigin": ""
+        }
+        document::Link {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Geist+Mono:wght@400..600&family=Geist:wght@400..600&display=swap"
+        }
+        document::Link {
+            rel: "stylesheet", href: asset!("/assets/tailwind.css")
+        }
         Router::<crate::router::Route> {}
     }
 }
