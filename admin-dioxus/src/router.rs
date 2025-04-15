e8a542a5749236975b2a5b061be4e208854a0b56
hmziqrs <hmziqrs@gmail.com>
2025-04-15 21:37:54 +0500

    Container NavBar working

--- admin-dioxus/src/router.rs
+++ admin-dioxus/src/router.rs
@@ -1,7 +1,8 @@
 use dioxus::prelude::*;
 use std::sync::LazyLock;
 
-use crate::containers::AuthGuard;
+use crate::containers::AuthGuardContainer;
+use crate::containers::NavBarContainer;
 use crate::screens::HomeScreen;
 use crate::screens::LoginScreen;
 use crate::screens::AddBlogScreen;
@@ -12,8 +13,8 @@ use crate::screens::AddUserScreen;
 #[derive(Debug, Clone, Routable, PartialEq)]
 #[rustfmt::skip]
 pub enum Route {
-    #[layout(AuthGuard)]
-    #[layout(NavBar)]
+    #[layout(AuthGuardContainer)]
+    #[layout(NavBarContainer)]
     #[route("/")]
     HomeScreen {},
 
@@ -35,34 +36,3 @@ pub enum Route {
 pub static OPEN_ROUTES: LazyLock<Vec<Route>> = LazyLock::new(|| vec![
     Route::LoginScreen {},
 ]);
-
-#[component]
-fn NavBar() -> Element {
-    rsx! {
-        div { class: "bg-zinc-800 text-white py-4 px-6 flex justify-between items-center shadow-lg",
-            h1 { class: "text-2xl font-bold tracking-tight", "Admin Dioxus" }
-            ul { class: "flex space-x-6",
-                li { class: "hover:text-zinc-300 transition-colors duration-200",
-                    Link { class: "font-medium", to: Route::HomeScreen {}, "Home" }
-                }
-                li { class: "hover:text-zinc-300 transition-colors duration-200",
-                    Link { class: "font-medium", to: Route::LoginScreen {}, "Login" }
-                }
-                li { class: "hover:text-zinc-300 transition-colors duration-200",
-                    Link { class: "font-medium", to: Route::AddBlogScreen {}, "New Blog Post" }
-                }
-                li { class: "hover:text-zinc-300 transition-colors duration-200",
-                    Link { class: "font-medium", to: Route::AddCategoryScreen {}, "New Category" }
-                }
-                li { class: "hover:text-zinc-300 transition-colors duration-200",
-                    Link { class: "font-medium", to: Route::AddTagScreen {}, "New Tag" }
-                }
-                li { class: "hover:text-zinc-300 transition-colors duration-200",
-                    Link { class: "font-medium", to: Route::AddUserScreen {}, "New User" }
-                }
-            }
-        }
-        Outlet::<Route> {}
-    }
-}
-
