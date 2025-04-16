ce277fb816a857cd61b42c77725a109302466a33
hmziqrs <hmziqrs@gmail.com>
2025-04-16 12:12:01 +0500

    Enhance dark mode support and improve UI components with new utility classes and styles

--- admin-dioxus/src/screens/home.rs
+++ admin-dioxus/src/screens/home.rs
@@ -1,12 +1,9 @@
 use dioxus::prelude::*;
 
-
 #[component]
 pub fn HomeScreen() -> Element {
-
-
     rsx! {
-        div {
+        div { class: "",
             h1 { "Welcome to Dioxus!" }
             p { "This is a simple example of a Dioxus application." }
             button { class: "btn btn-primary", onclick: move |_| {}, "Simulate" }
