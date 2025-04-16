19882fd769b911e5f5834bd4b28a9fcf2ba06cea
hmziqrs <hmziqrs@gmail.com>
2025-04-16 09:16:27 +0500

    Enhance NavBar with dark mode detection and icon size adjustments

--- admin-dioxus/src/containers/nav_bar/nav_bar.rs
+++ admin-dioxus/src/containers/nav_bar/nav_bar.rs
@@ -1,18 +1,32 @@
+use dioxus::logger::tracing;
 use dioxus::prelude::*;
-use hmziq_dioxus_free_icons::{Icon, IconShape};
+use hmziq_dioxus_free_icons::{Icon};
 // Import the specific lucide icons we need
 use hmziq_dioxus_free_icons::icons::ld_icons::{
-    LdMenu, LdBell, LdSearch, LdSettings, LdChevronLeft, LdChevronRight
+    LdBell, LdChevronLeft, LdChevronRight, LdMenu, LdMoon, LdSearch, LdSettings, LdSun,
 };
 
-use crate::{router::Route, store::use_auth, components::Sidebar};
+use crate::{components::Sidebar, router::Route, store::use_auth};
 
 #[component]
 pub fn NavBarContainer() -> Element {
     let auth_store = use_auth();
     let auth_user = auth_store.user.read();
     let mut sidebar_open = use_signal(|| false); // Default to open on desktop
-    
+    let mut dark_theme = use_signal(|| false); // Default to light mode
+
+    use_effect(move || {
+        spawn(async move {
+            let is_dark =
+                document::eval("return document.documentElement.classList.contains('dark');")
+                    .await
+                    .unwrap()
+                    .to_string();
+            tracing::info!("Dark mode is: {}", is_dark);
+            dark_theme.set(is_dark.parse::<bool>().unwrap_or(false));
+        });
+    });
+
     // Toggle sidebar handler
     let toggle_sidebar = move |_: MouseEvent| {
         sidebar_open.toggle();
@@ -21,7 +35,7 @@ pub fn NavBarContainer() -> Element {
     if auth_user.is_none() {
         return rsx! {
             Outlet::<Route> {}
-        }
+        };
     }
 
     rsx! {
@@ -47,19 +61,19 @@ pub fn NavBarContainer() -> Element {
                             button {
                                 class: "rounded-md p-2 text-gray-400 hover:bg-zinc-700 hover:text-white sm:hidden",
                                 onclick: move |_| sidebar_open.set(true),
-                                Icon { icon: LdMenu, width: 20, height: 20 }
+                                div { class: "w-4 h-4",
+                                    Icon { icon: LdMenu }
+                                }
                             }
                             // Desktop sidebar toggle button
                             button {
                                 class: "hidden rounded-md p-2 text-gray-400 hover:bg-zinc-700 hover:text-white sm:flex",
                                 onclick: toggle_sidebar,
-                                if *sidebar_open.read() {
-                                    Icon { icon: LdChevronLeft }
-                                } else {
-                                    Icon {
-                                        icon: LdChevronRight,
-                                        width: 20,
-                                        height: 20,
+                                div { class: "w-4 h-4",
+                                    if *sidebar_open.read() {
+                                        Icon { icon: LdChevronLeft }
+                                    } else {
+                                        Icon { icon: LdChevronRight }
                                     }
                                 }
                             }
@@ -69,11 +83,11 @@ pub fn NavBarContainer() -> Element {
                         div { class: "hidden md:block flex-1 px-4 lg:px-6",
                             div { class: "relative max-w-md",
                                 div { class: "pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3",
-                                    Icon {
-                                        icon: LdSearch,
-                                        width: 16,
-                                        height: 16,
-                                        class: "text-zinc-400",
+                                    div { class: "w-4 h-4",
+                                        Icon {
+                                            icon: LdSearch,
+                                            class: "text-zinc-400",
+                                        }
                                     }
                                 }
                                 input {
@@ -87,10 +101,25 @@ pub fn NavBarContainer() -> Element {
                         // Right side nav items
                         div { class: "flex items-center space-x-4",
                             button { class: "rounded-full p-1 text-zinc-400 hover:bg-zinc-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-zinc-800",
-                                Icon { icon: LdBell, width: 20, height: 20 }
+                                div { class: "w-4 h-4",
+                                    Icon { icon: LdBell }
+                                }
                             }
-                            button { class: "rounded-full p-1 text-zinc-400 hover:bg-zinc-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-zinc-800",
-                                Icon { icon: LdSettings, width: 20, height: 20 }
+                            button {
+                                onclick: move |_| {
+                                    spawn(async move {
+                                        _ = document::eval("document.documentElement.classList.toggle('dark');")
+                                            .await;
+                                    });
+                                },
+                                class: "rounded-full p-1 text-zinc-400 hover:bg-zinc-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-zinc-800",
+                                div { class: "w-4 h-4",
+                                    if *dark_theme.read() {
+                                        Icon { icon: LdSun }
+                                    } else {
+                                        Icon { icon: LdMoon }
+                                    }
+                                }
                             }
                             // User profile
                             div { class: "relative ml-3",
