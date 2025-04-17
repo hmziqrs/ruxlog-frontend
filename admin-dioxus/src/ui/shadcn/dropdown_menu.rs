43fbfe78af406585b07613bde74a59f0ce81cbdc
hmziqrs <hmziqrs@gmail.com>
2025-04-17 10:32:55 +0500

    feat: Enhance styling in BlogListScreen and DropdownMenu components; add new utility classes and improve delete item visibility

--- admin-dioxus/src/ui/shadcn/dropdown_menu.rs
+++ admin-dioxus/src/ui/shadcn/dropdown_menu.rs
@@ -74,7 +74,10 @@ pub fn DropdownMenuTrigger(props: DropdownMenuProps) -> Element {
 #[component]
 pub fn DropdownMenuContent(props: DropdownMenuProps) -> Element {
     let open = use_context::<Signal<DropdownContext>>();
-    let mut class = vec!["bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] overflow-hidden rounded-md border p-1 shadow-md absolute".to_string()];
+    let mut class = vec![
+        "border-zinc-200 dark:border-zinc-800 bg-white dark:bg-zinc-950".to_string(),
+        "dropdown-menu-content bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] overflow-hidden rounded-md border p-1 shadow-md absolute".to_string()
+    ];
 
     if let Some(custom_class) = props.class {
         class.push(custom_class);
