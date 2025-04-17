268e5aa92e652689575b30c5adb56ac3a2b358b7
hmziqrs <hmziqrs@gmail.com>
2025-04-17 05:21:28 +0500

    feat: Enhance Avatar component with loading and error states; update styles and context management

--- admin-dioxus/src/ui/shadcn/avatar.rs
+++ admin-dioxus/src/ui/shadcn/avatar.rs
@@ -1,4 +1,4 @@
-use dioxus::prelude::*;
+use dioxus::{logger::tracing, prelude::*};
 
 #[derive(Props, PartialEq, Clone)]
 pub struct AvatarProps {
@@ -27,10 +27,17 @@ pub struct AvatarFallbackProps {
     children: Element,
 }
 
+#[derive(PartialEq, Eq)]
+pub enum AvatarImageStatus {
+    Loading,
+    Error,
+    Success,
+}
 
 
 #[component]
 pub fn Avatar(props: AvatarProps) -> Element {
+    use_context_provider(|| Signal::new(AvatarImageStatus::Loading));
     let mut class = vec!["relative flex size-8 shrink-0 overflow-hidden rounded-full".to_string()];
     
     if let Some(custom_class) = props.class {
@@ -45,17 +52,37 @@ pub fn Avatar(props: AvatarProps) -> Element {
 #[component]
 pub fn AvatarImage(props: AvatarImageProps) -> Element {
     let mut class = vec!["aspect-square size-full".to_string()];
+    let mut status = use_context::<Signal<AvatarImageStatus>>();
+
+    let is_error = *status.read() == AvatarImageStatus::Error;
+    let is_loading = *status.read() == AvatarImageStatus::Loading;
     
     if let Some(custom_class) = props.class {
         class.push(custom_class);
     }
 
+
+    if is_error || props.src.is_empty() {
+        class.push("hidden".to_string());
+    } else {
+        class.push("block".to_string());
+    }
+
     rsx! {
         img {
+            onload: move |_| {
+                status.set(AvatarImageStatus::Success);
+            },
+            onerror: move |_| {
+                status.set(AvatarImageStatus::Error);
+            },
             "data-slot": "avatar-image",
             class: class.join(" "),
             src: props.src,
             alt: props.alt,
+        }
+        if is_loading {
+            div { class: "loading loading-spinner loading-xs absolute" }
         }
     }
 }
