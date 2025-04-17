2a02a75df5a8c72e53dceaa0736b4e8382771e33
hmziqrs <hmziqrs@gmail.com>
2025-04-17 20:01:12 +0500

    park

--- admin-dioxus/src/components/mod.rs
+++ admin-dioxus/src/components/mod.rs
@@ -2,10 +2,10 @@ mod input;
 pub mod shadcn_ui;
 mod sidebar;
 mod toast;
-mod command;
+// mod command;
 
 pub use input::*;
 pub use sidebar::*;
 pub use toast::*;
-pub use command::*;
+// pub use command::*;
 // pub use shadcn_ui::*;
