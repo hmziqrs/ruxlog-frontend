01d5fb2a7651a4f11da007e6238d77e400bcbb16
hmziqrs <hmziqrs@gmail.com>
2025-04-18 20:14:13 +0500

    Park

--- admin-dioxus/src/screens/category_list/category_list.rs
+++ admin-dioxus/src/screens/category_list/category_list.rs
@@ -1,8 +1,27 @@
 use dioxus::prelude::*;
 
+use crate::ui::custom::*;
+
 #[component]
 pub fn CategoryListScreen() -> Element {
     rsx! {
         div { "Category List (placeholder)" }
+        Command {
+            CommandInput {}
+            CommandList {
+                CommandEmpty { "No results found." }
+                CommandGroup { heading: "Suggestions",
+                    CommandItem { value: "calendar", "Calendar" }
+                    CommandItem { value: "search", "Search Emoji" }
+                    CommandItem { value: "calculator", "Calculator" }
+                }
+                CommandSeparator {}
+                CommandGroup { heading: "Settings",
+                    CommandItem { value: "profile", "Profile" }
+                    CommandItem { value: "billing", "Billing" }
+                    CommandItem { value: "settings", "Settings" }
+                }
+            }
+        }
     }
 }
