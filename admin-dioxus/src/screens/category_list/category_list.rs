cdafdab3b3e9b3205365b6453e72f57986f9919f
hmziqrs <hmziqrs@gmail.com>
2025-04-21 01:42:20 +0500

    Refactor Tabs component: update TabsProps to use state management and adjust TabsState visibility

--- admin-dioxus/src/screens/category_list/category_list.rs
+++ admin-dioxus/src/screens/category_list/category_list.rs
@@ -1,46 +1,43 @@
 use dioxus::prelude::*;
 
-use crate::ui::{custom::cmdk::*, shadcn::{TabItem, Tabs}};
+use crate::ui::{
+    custom::cmdk::*,
+    shadcn::{TabItem, Tabs, TabsState},
+};
 
 #[component]
 pub fn CategoryListScreen() -> Element {
-        let tabs = vec![
-        TabItem::new(
-            "First Tab".to_string(), 
-            false
-        ),
-        TabItem::new(
-            "Second Tab".to_string(), 
-            false
-        ),
-        TabItem::new(
-            "Disabled Tab".to_string(), 
-            true
-        ),
+    let tabs = vec![
+        TabItem::new("First Tab".to_string(), false),
+        TabItem::new("Second Tab".to_string(), false),
+        TabItem::new("Disabled Tab".to_string(), true),
     ];
-    
-    let groups = vec!["Settings".to_string(), "Suggestions".to_string(), "Other".to_string()];
-    
+
+    let groups = vec![
+        "Settings".to_string(),
+        "Suggestions".to_string(),
+        "Other".to_string(),
+    ];
+
     let data = vec![
         CommandListItem::new("settings".to_string(), "Profile".to_string(), false),
         CommandListItem::new("settings".to_string(), "Billing".to_string(), false),
         CommandListItem::new("settings".to_string(), "Settings".to_string(), false),
         CommandListItem::new("settings".to_string(), "Display".to_string(), false),
-        
         //
         CommandListItem::new("suggestions".to_string(), "Calendar".to_string(), false),
         CommandListItem::new("suggestions".to_string(), "Emoji".to_string(), false),
         CommandListItem::new("suggestions".to_string(), "Calculator".to_string(), true),
-        
         //
         CommandListItem::new("other".to_string(), "Camera".to_string(), true),
         CommandListItem::new("other".to_string(), "Video".to_string(), true),
-        
     ];
+
+    let tab_state: Signal<TabsState> = use_signal(|| TabsState::new(tabs, 0));
 
     rsx! {
         div { "Category List (placeholder)" }
-        Tabs { tabs, default_index: 0 }
+        Tabs { state: tab_state }
         Cmdk { groups, data, reset_on_select: true }
     }
 }
