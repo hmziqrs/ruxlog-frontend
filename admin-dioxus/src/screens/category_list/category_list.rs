0e946e9cc6705e3e3df83d5e7f56126e638bb430
hmziqrs <hmziqrs@gmail.com>
2025-04-20 22:32:56 +0500

    Enhance Cmdk component: add keyboard navigation for item selection; refactor CommandListItem structure to include index; update state management for active index.

--- admin-dioxus/src/screens/category_list/category_list.rs
+++ admin-dioxus/src/screens/category_list/category_list.rs
@@ -7,19 +7,19 @@ pub fn CategoryListScreen() -> Element {
     let groups = vec!["Settings".to_string(), "Suggestions".to_string(), "Other".to_string()];
     
     let data = vec![
-        CommandListItem::new("settings".to_string(), "Profile".to_string(), None, false),
-        CommandListItem::new("settings".to_string(), "Billing".to_string(), None, false),
-        CommandListItem::new("settings".to_string(), "Settings".to_string(), None, false),
-        CommandListItem::new("settings".to_string(), "Display".to_string(), None, false),
+        CommandListItem::new("settings".to_string(), "Profile".to_string(), false),
+        CommandListItem::new("settings".to_string(), "Billing".to_string(), false),
+        CommandListItem::new("settings".to_string(), "Settings".to_string(), false),
+        CommandListItem::new("settings".to_string(), "Display".to_string(), false),
         
         //
-        CommandListItem::new("suggestions".to_string(), "Calendar".to_string(), None, false),
-        CommandListItem::new("suggestions".to_string(), "Emoji".to_string(), None, false),
-        CommandListItem::new("suggestions".to_string(), "Calculator".to_string(), None, true),
+        CommandListItem::new("suggestions".to_string(), "Calendar".to_string(), false),
+        CommandListItem::new("suggestions".to_string(), "Emoji".to_string(), false),
+        CommandListItem::new("suggestions".to_string(), "Calculator".to_string(), true),
         
         //
-        CommandListItem::new("other".to_string(), "Camera".to_string(), None, true),
-        CommandListItem::new("other".to_string(), "Video".to_string(), None, true),
+        CommandListItem::new("other".to_string(), "Camera".to_string(), true),
+        CommandListItem::new("other".to_string(), "Video".to_string(), true),
         
     ];
 
