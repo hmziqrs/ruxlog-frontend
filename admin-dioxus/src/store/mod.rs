a17a12097a1faff4e0b3e592456c670927a649fc
hmziqrs <hmziqrs@gmail.com>
2025-08-08 16:53:13 +0500

    Remove commented code and reorder imports for clarity

--- admin-dioxus/src/store/mod.rs
+++ admin-dioxus/src/store/mod.rs
@@ -1,13 +1,13 @@
 mod auth;
-mod post;
 mod category;
+mod lib;
+mod post;
 mod tag;
 mod user;
-mod lib;
 
 pub use auth::*;
-pub use post::*;
 pub use category::*;
+pub use lib::*;
+pub use post::*;
 pub use tag::*;
 pub use user::*;
-pub use lib::*;
