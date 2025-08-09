b38112eb86cff20575247b328e4f690b1984a776
hmziqrs <hmziqrs@gmail.com>
2025-08-09 12:03:40 +0500

    refactor: reorganize store modules and update sdk dependencies

--- admin-dioxus/src/screens/tags/list/mod.rs
+++ admin-dioxus/src/screens/tags/list/mod.rs
@@ -1,7 +1,10 @@
 use dioxus::prelude::*;
 
+use crate::store::use_tag;
+
 #[component]
 pub fn TagsListScreen() -> Element {
+    let tags = use_tag();
     rsx! {
         div { "Tag List (placeholder)" }
     }
