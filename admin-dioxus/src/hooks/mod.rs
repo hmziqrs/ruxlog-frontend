36d077c5d9c48c77dddade3ff47c44a6c672e192
hmziqrs <hmziqrs@gmail.com>
2025-08-11 03:20:22 +0500

    feat: add category edit screen and form functionality

--- admin-dioxus/src/hooks/mod.rs
+++ admin-dioxus/src/hooks/mod.rs
@@ -1,7 +1,9 @@
 mod use_form;
 mod use_previous;
 mod use_tag_view;
+mod use_category_view;
 
 pub use use_form::*;
 pub use use_previous::use_previous;
 pub use use_tag_view::{use_tag_view, TagViewState};
+pub use use_category_view::{use_category_view, CategoryViewState as CategoryDetailViewState};
